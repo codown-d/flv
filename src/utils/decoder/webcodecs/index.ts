2fd9d5326465f83ab616fe9ca463c17743fc1507
DESKTOP-8TGIBBQ\zhanghe <123456@qq.com>
2024-11-05 02:11:10 +0800

    no message

--- src/utils/decoder/webcodecs/index.ts
+++ src/utils/decoder/webcodecs/index.ts
@@ -1,47 +1,98 @@
-import VideoDecoderWorker from './videoDecoder?worker'
-import videoDecoder, { VideoWorkerDecoder as VideoDecoder, PostMessageEventMap } from './videoDecoder'
-import { DemuxedSourceData, MediaType } from '../../demuxer';
-import { AudioMeta, VideoMeta } from '../../demuxer/MSEFlvDemuxer';
+import VideoDecoderWorker from "./videoDecoder?worker";
+import videoDecoder, {
+  VideoWorkerDecoder as VideoDecoder,
+  PostMessageEventMap,
+} from "./videoDecoder";
+import { DemuxedSourceData, MediaType } from "../../demuxer";
+import { AudioMeta, VideoMeta } from "../../demuxer/MSEFlvDemuxer";
 enum MediaSelectType {
   NULL = 0,
   AUDIO = 1,
   VIDEO = 2,
-  ALL = 3
+  ALL = 3,
 }
 export default class Decoder {
-  useWorker?: boolean
-  constructor() {
-  }
+  useWorker?: boolean;
+
+  videoDecoder?: VideoDecoder; // 视频解码器实例
+  audioDecoder?: any; // 可能需要的音频解码器实例，视具体实现而定
+  constructor() {}
   async init(useWorker = true, decodeType = MediaSelectType.ALL) {
-    this.useWorker = useWorker
+    this.useWorker = useWorker;
     if (useWorker) {
       if (decodeType & MediaSelectType.VIDEO) {
         // code
+        this.videoDecoder = new VideoDecoder(); // 实例化视频解码器
+        console.log("videoDecoder", this.videoDecoder)
+        // 设置工作线程消息处理
+        this.videoDecoder.onmessage = (event: MessageEvent<PostMessageEventMap>) => {
+          const { type, value } = event.data;
+          this.onMessage(type, value); // 处理来自工作线程的消息
+        };
+  
+        // 发送配置到工作线程
+        const config = {
+          codec: 'avc1.64001E', // 示例 H.264 编码
+          offscreen: new OffscreenCanvas(640, 480), // 创建 OffscreenCanvas
+          spspps: new Uint8Array(), // 替换为实际的 SPS/PPS 数据
+        };
+        this.videoDecoder.postMessage({ type: 'configure', value: config }); // 配置工作线程
       }
     }
   }
   decode(type: MediaType, data: DemuxedSourceData[]) {
-    switch(type) {
-      case 'video':
+    switch (type) {
+      case "video":
         {
           // code
+          if (this.videoDecoder) {
+            this.videoDecoder.decode(data); // 使用工作线程解码视频数据
+          }
+          break;
         }
-        break
+        break;
     }
   }
   setMeta(type: any, data: any): void {
-    switch(type) {
-      case 'video':
+    switch (type) {
+      case "video":
         {
           // code
+          // code
+          if (this.videoDecoder) {
+            this.videoDecoder.configure({
+              // 配置视频解码器
+              codec: data.codec,
+              codedWidth: data.codecWidth,
+              codedHeight: data.codecHeight,
+              description: data.spspps, // 假设这是 SPS/PPS 数据
+            });
+          }
+          break;
         }
-        break
     }
   }
   onMessage(type: any, value: any): void {
     // code
+    switch (type) {
+      case "data": {
+        // 处理从工作线程返回的解码数据
+        const { img, timestamp } = value;
+        // TODO: 将解码后的图像绘制到画布或进行其他处理
+        break;
+      }
+      // 处理其他消息类型...
+    }
   }
   stop() {
     // code
+    // 停止工作线程和解码器
+    if (this.videoDecoder) {
+      this.videoDecoder.stop(); // 停止视频解码器
+    }
+    // 如果有音频解码器，也需要停止
+    if (this.audioDecoder) {
+      this.audioDecoder.stop(); // 停止音频解码器
+    }
   }
 }
--- src/utils/decoder/webcodecs/videoDecoder.ts
+++ src/utils/decoder/webcodecs/videoDecoder.ts
@@ -1,37 +1,91 @@
 // @ts-check
 /// <reference lib="webworker"/>
 
-import { DemuxedSourceData } from "../../demuxer"
-import { isInWorker } from "../../tool"
+import { DemuxedSourceData } from "../../demuxer";
+import { isInWorker } from "../../tool";
 
 export interface OnMessageEventMap {
-  data: DemuxedSourceData[]
-  configure: VideoDecoderConfig & {offscreen: OffscreenCanvas;spspps: Uint8Array}
+  data: DemuxedSourceData[];
+  configure: VideoDecoderConfig & {
+    offscreen: OffscreenCanvas;
+    spspps: Uint8Array;
+  };
 }
 
 export interface PostMessageEventMap {
   data: {
-    img: ImageBitmap,
-    timestamp: number
-  }
+    img: ImageBitmap;
+    timestamp: number;
+  };
 }
-type WorkerGlobal = typeof globalThis
+type WorkerGlobal = typeof globalThis;
 
 export class VideoWorkerDecoder {
-  decoder?: VideoDecoder
-  spspps?: Uint8Array
-  context?: Window & WorkerGlobal
-  offscreen?: OffscreenCanvas
-  offscreenCtx?: OffscreenCanvasRenderingContext2D
+  decoder?: VideoDecoder;
+  spspps?: Uint8Array;
+  context?: Window & WorkerGlobal;
+  offscreen?: OffscreenCanvas;
+  offscreenCtx?: OffscreenCanvasRenderingContext2D;
   constructor(context?: Window & WorkerGlobal) {
     // code
+    this.context = context;
+    if (this.context) {
+      this.offscreen = new OffscreenCanvas(640, 480); // 创建 OffscreenCanvas
+      this.offscreenCtx = this.offscreen.getContext("2d"); // 获取绘图上下文
+    }
   }
   onMessage(type: any, value: any): void {
     // code
+    console.log("onMessage", type, value)
+    switch (type) {
+      case "configure": {
+        this.spspps = value.spspps; // 保存 SPS/PPS 数据
+        this.decoder = new VideoDecoder({
+          output: (imageBitmap) => {
+            // 发送解码后的图像位图
+            this.postMessage("data", {
+              img: imageBitmap,
+              timestamp: performance.now(),
+            });
+          },
+          error: (e) => {
+            console.error("Decoder error:", e);
+          },
+        });
+        // 配置解码器
+        this.decoder.configure({
+          codec: "avc1.64001E", // H.264 编码
+          codedWidth: 640,
+          codedHeight: 480,
+          description: this.spspps, // 设置 SPS/PPS 数据
+          // 如果有其他需要的配置项，可以在这里添加
+        });
+        break;
+      }
+      case "decode": {
+        // 解码收到的数据
+        if (this.decoder) {
+          this.decoder.decode(value);
+        }
+        break;
+      }
+      case "flush": {
+        // 刷新解码器，处理剩余数据
+        if (this.decoder) {
+          this.decoder.flush();
+        }
+        break;
+      }
+    }
   }
   postMessage(type: any, value: any): void {
     // code
+    if (this.context) {
+      this.context.postMessage({ type, value });
+    } else {
+      console.warn("No context available for postMessage.");
+    }
   }
 }
 
-export default new VideoWorkerDecoder(isInWorker() ? self : undefined)
+export default new VideoWorkerDecoder(isInWorker() ? self : undefined);
